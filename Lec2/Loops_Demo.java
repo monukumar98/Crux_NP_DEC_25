c77040478defbffbbc5f3af5a7834add0a5c981c
monukumar98 <monu012as@gmail.com>
2025-12-28 19:46:21 +0530

    Pattern

---
+++ Lec2/Loops_Demo.java
@@ -0,0 +1,20 @@
+package Lec2;
+
+public class Loops_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("Hello");
+//		System.out.println("Hello");
+//		System.out.println("Hello");
+//		System.out.println("Hello");
+//		System.out.println("Hello");
+		int i = 1;
+		while (i <= 5) {
+			System.out.print(i+" ");
+			i++;	
+		}
+
+	}
+
+}
