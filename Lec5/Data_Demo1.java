35c155bcc281014d7bb45b51a755b7072bf7dfcc
monukumar98 <monu012as@gmail.com>
2026-01-10 19:38:18 +0530

    Data Types

---
+++ Lec5/Data_Demo1.java
@@ -0,0 +1,26 @@
+package Lec5;
+
+import java.util.Scanner;
+
+public class Data_Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// byte b = (byte)(428);// -128 to 127
+		byte b = (byte) (300);// -128 to 127
+		short s = 5;
+		int i = 5;
+		long l = 5;
+		System.out.println(b);
+		byte b1 = -5;
+//		Scanner sc = new Scanner(System.in);
+//		b=sc.nextByte();
+//		s=sc.nextShort();
+//		i=sc.nextInt();
+//		l=sc.nextLong();
+		long l1 = 2366467899l;
+		
+
+	}
+
+}
---
+++ Lec5/Data_Demo2.java
@@ -0,0 +1,17 @@
+package Lec5;
+
+import java.util.Scanner;
+
+public class Data_Demo2 {
+	public static void main(String[] args) {
+		double d = 78.9;// double
+		float f = 56.8f;
+		Scanner sc = new Scanner(System.in);
+		f=sc.nextFloat();
+		d=sc.nextDouble();
+		boolean b1=true;
+		boolean b2=false;
+		b1=sc.nextBoolean();
+	}
+
+}
