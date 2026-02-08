3b964ff5d87fd8feb31b439b5346885331942cca
monukumar98 <monu012as@gmail.com>
2026-02-08 10:24:36 +0530

    Binary Search

---
+++ Lec10/First_Bad_Version.java
@@ -0,0 +1,29 @@
+package Lec10;
+
+public class First_Bad_Version {
+	public static void main(String[] args) {
+
+	}
+
+	public int firstBadVersion(int n) {
+		int lo = 1;
+		int hi = n;
+		int ans = 0;
+		while (lo <= hi) {
+			int mid = (lo + hi) / 2;
+			if (isBadVersion(mid)) {
+				ans = mid;
+				hi = mid - 1;
+			} else {
+				lo = mid + 1;
+			}
+		}
+		return ans;
+
+	}
+
+	private boolean isBadVersion(int mid) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
---
+++ Lec10/Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,42 @@
+package Lec10;
+
+public class Search_in_Rotated_Sorted_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 4, 5, 6, 7, 0, 1, 2 };
+		int target = 0;
+
+	}
+
+	public static int Search(int[] arr, int target) {
+		int lo = 0;
+		int hi = arr.length - 1;
+		while (lo <= hi) {
+			int mid = (lo + hi) / 2;
+			if (arr[mid] == target) {
+				return mid;
+			}
+			if (arr[mid] >= arr[lo]) {// mera mid uppar line pe hai
+				if(arr[lo]<=target && arr[mid]>target) {
+					hi=mid-1;
+				}
+				else {
+					lo=mid+1;
+				}
+
+			} else {// mera mid lower line pe hai
+				if(arr[mid]<target && arr[hi]>=target) {
+					lo=mid+1;
+				}
+				else {
+					hi=mid-1;
+				}
+
+			}
+		}
+
+		return -1;
+	}
+
+}
