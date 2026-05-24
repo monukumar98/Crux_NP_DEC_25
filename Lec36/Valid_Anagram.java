24a07d421409ce0624a0ad457a5b33bca08fb4af
monukumar98 <monu012as@gmail.com>
2026-05-24 19:47:43 +0530

    Map Implemention

--- Lec36/Valid_Anagram.java
+++ Lec36/Valid_Anagram.java
@@ -20,8 +20,8 @@ public class Valid_Anagram {
 				map.put(ch, 1);
 			}
 		}
-		for (int i = 0; i < s.length(); i++) {
-			char ch = s.charAt(i);
+		for (int i = 0; i < t.length(); i++) {
+			char ch = t.charAt(i);
 			if (!map.containsKey(ch) || map.get(ch) == 0) {
 				return false;
 			}
